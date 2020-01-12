d723547c1f1661ed9fbe5c420fca986d23ed600a
Carson Jobe <cdjobe@gmail.com>
2020-01-12 14:34:00 -0600

    Ninja Gold
    Ninja Gold

--- Flask/Ninja-Gold/ng_server.py
+++ Flask/Ninja-Gold/ng_server.py
@@ -1,9 +1,28 @@
 from flask import Flask, redirect, render_template, request, session
 app = Flask(__name__)
-
+app.secret_key = 'secret'
 @app.route('/')
 def index():
+    if 'gold' in session:
+        session['amount_of_gold'] = session.get('gold_amount')
+    else:
+        session['amount_of_gold'] = 0 
+        session['activities'] = []
     return render_template('/index.html')
+
+@app.route('/process_gold', methods=['POST'])
+def process_gold():
+    choice = request.form['choice']
+    print(choice)
+    if choice == 'farm':
+        randNum = random.randint(10, 20)
+    elif choice == 'cave':
+        randNum = random.randint(5, 10)
+    elif choice == 'house':
+        randNum = random.randint(2, 5)
+    elif choice == 'casino':
+        randNum = random.randint(-50, 0)
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
